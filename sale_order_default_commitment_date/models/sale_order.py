be2fa1f88b99e961a6fa9be5153577ad2db92dca
Janik von Rotz <login@janikvonrotz.ch>
2021-09-24 16:25:24 +0200

    [Fix] Set timezone formatting

--- sale_order_default_commitment_date/models/sale_order.py
+++ sale_order_default_commitment_date/models/sale_order.py
@@ -2,6 +2,7 @@ from odoo import api, fields, models, _
 import logging
 _logger = logging.getLogger(__name__)
 from datetime import timedelta, datetime
+from pytz import timezone, UTC
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
@@ -14,11 +15,19 @@ class SaleOrder(models.Model):
             "this date rather than product lead times.")
 
     def _get_default_commitment_date(self):
-        _logger.info(f"Current time is: {datetime.now()}")
+        
+        # Get now with timezone
+        now = datetime.now().replace(tzinfo=UTC).astimezone(timezone(self.env.user.tz or 'UTC')).replace(tzinfo=None)
+
         # Add 1 day before noon and 2 days after
-        if datetime.now().hour < 12:
-            commitment_date = datetime.now() + timedelta(days=1)
+        if now.hour < 12:
+            commitment_date = now + timedelta(days=1)
         else:
-            commitment_date = datetime.now() + timedelta(days=2)
-        # Set time and return
-        return commitment_date.replace(hour=9, minute=57)
+            commitment_date = now + timedelta(days=2)
+
+        # Set default time
+        commitment_date = commitment_date.replace(hour=7, minute=57)
+
+        # _logger.info(f'{commitment_date}')
+
+        return commitment_date
