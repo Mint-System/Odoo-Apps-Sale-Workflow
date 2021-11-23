ca92be4929bd380155aba746b521f0c2df00c54d
Janik von Rotz <login@janikvonrotz.ch>
2021-11-23 11:05:06 +0100

    fix stock picking update

--- sale_order_delivery_note/__manifest__.py
+++ sale_order_delivery_note/__manifest__.py
@@ -8,7 +8,7 @@
     'author': 'Mint System GmbH, Odoo Community Association (OCA)',
     'website': 'https://www.mint-system.ch',
     'category': 'Sale',
-    'version': '14.0.1.0.0',
+    'version': '14.0.1.1.0',
     'license': 'AGPL-3',
     
     'depends': ['sale_management', 'stock_delivery_note'],
--- sale_order_delivery_note/models/sale_order.py
+++ sale_order_delivery_note/models/sale_order.py
@@ -1,4 +1,6 @@
 from odoo import api, fields, models
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class SaleOrder(models.Model):
@@ -6,8 +8,14 @@ class SaleOrder(models.Model):
 
     delivery_note = fields.Text()
 
-    @api.multi
-    def _prepare_procurement_values(self, group_id=False):
-        res = super(SaleOrder, self)._prepare_procurement_values(group_id)
-        res.update({'delivery_note': self.delivery_note})
+    def action_confirm(self):
+        res = super(SaleOrder, self).action_confirm()
+        for picking in self.picking_ids:
+            picking.write({'delivery_note': self.delivery_note})
         return res
+
+    @api.onchange('delivery_note')
+    def _onchange_delivery_note(self):
+        for rec in self:
+            pickings = self.env['stock.picking'].search([('id', 'in', rec.picking_ids.ids)])
+            pickings.write({'delivery_note': rec.delivery_note})
