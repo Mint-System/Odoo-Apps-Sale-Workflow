3a72c10cddb7c3a02a18602c46a9f5fd53066a06
Janik von Rotz <login@janikvonrotz.ch>
2021-11-25 17:56:10 +0100

    [IMP] sale_blanket_order_stock_terms: init

---
+++ sale_blanket_order_stock_terms/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Sale Blanket Order Stock Terms",
+    "summary": """
+        Set incoterm and shipping policy on blanket order.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Uncategorized",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["sale_blanket_order", "sale_stock"],
+    "data": ["views/sale_blanket_order.xml",],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
---
+++ sale_blanket_order_stock_terms/models/blanket_order.py
@@ -0,0 +1,18 @@
+from odoo import fields, models
+
+
+class BlanketOrder(models.Model):
+    _inherit = "sale.blanket.order"
+
+    incoterm = fields.Many2one(
+        "account.incoterms",
+        help="International Commercial Terms are a series of predefined commercial terms used in international transactions.",
+    )
+    picking_policy = fields.Selection(
+        [("direct", "As soon as possible"), ("one", "When all products are ready")],
+        required=True,
+        readonly=True,
+        default="direct",
+        states={"draft": [("readonly", False)]},
+        help="If you deliver all products at once, the delivery order will be scheduled based on the greatest product lead time. Otherwise, it will be based on the shortest.",
+    )
