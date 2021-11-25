1024badd6c7f8c0239d687c2240a4045bedb87c4
Janik von Rotz <login@janikvonrotz.ch>
2021-11-25 16:17:03 +0100

    [IMP] sale_blanket_orderr_contact_person: init

---
+++ sale_blanket_order_contact_person/models/blanket_order.py
@@ -0,0 +1,7 @@
+from odoo import fields, models
+
+
+class BlanketOrder(models.Model):
+    _inherit = "sale.blanket.order"
+
+    partner_contact_id = fields.Many2one('res.partner', string="Contact Person")
--- sale_order_contact_person/models/sale_order.py
+++ sale_order_contact_person/models/sale_order.py
@@ -5,4 +5,3 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     partner_contact_id = fields.Many2one('res.partner', string="Contact Person")
-    # partner_contact_shipping_id = fields.Many2one('res.partner', string="Contact Person (Delivery)")
