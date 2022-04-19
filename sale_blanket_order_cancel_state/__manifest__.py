472192c08db0d96137c3d8e524517550a05cbc68
janikvonrotz <contact@janikvonrotz.ch>
2022-04-19 12:20:46 +0200

    [sale_blanket_order_cancel_state]: init

---
+++ sale_blanket_order_cancel_state/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Sale Blanket Order Cancel State",
+    "summary": """
+        Adds a cancel state to sale blanket order.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Sales",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["sale_blanket_order"],
+    "data": ["views/view_blanket_order_form.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
