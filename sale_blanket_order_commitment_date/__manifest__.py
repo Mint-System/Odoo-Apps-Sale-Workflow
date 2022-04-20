2de4d4b5f4131d2ab31ac01d77048ca24365487a
janikvonrotz <contact@janikvonrotz.ch>
2022-04-20 09:31:44 +0200

    [sale_blanket_order_commitment_date]: init

---
+++ sale_blanket_order_commitment_date/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Sale Blanket Order Commitment Date",
+    "summary": """
+        Copies the blanket order line scheduled date to sale line commitment date.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Uncategorized",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["sale_blanket_order","sale_order_line_date"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
