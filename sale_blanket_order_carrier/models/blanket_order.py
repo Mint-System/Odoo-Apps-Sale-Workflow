6aa9229ac7eccf5dd69d6f6849ea6efd78a5be79
janikvonrotz <contact@janikvonrotz.ch>
2022-05-04 17:12:51 +0200

    [sale_blanket_order_carrier] copy carrier from partner

--- sale_blanket_order_carrier/models/blanket_order.py
+++ sale_blanket_order_carrier/models/blanket_order.py
@@ -1,7 +1,12 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class BlanketOrder(models.Model):
     _inherit = "sale.blanket.order"
 
     carrier_id = fields.Many2one('delivery.carrier', string="Delivery Method", domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]", help="Fill this field if you plan to invoice the shipping based on picking.")
+
+    @api.onchange('partner_id')
+    def _onchange_carrier_id(self):
+        for order in self.filtered(lambda bo: not bo.carrier_id):
+            order.carrier_id = order.partner_id.property_delivery_carrier_id
