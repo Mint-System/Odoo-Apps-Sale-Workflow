29f4a9ef99fc9e38c222493c9da2ee7991ba92f2
janikvonrotz <contact@janikvonrotz.ch>
2022-05-17 08:46:43 +0200

    [sale_blanket_order_contact_person] set state

--- sale_blanket_order_contact_person/models/blanket_order.py
+++ sale_blanket_order_contact_person/models/blanket_order.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class BlanketOrder(models.Model):
     _inherit = "sale.blanket.order"
 
-    partner_contact_id = fields.Many2one('res.partner', string="Contact Person")
+    partner_contact_id = fields.Many2one('res.partner', string="Contact Person", states={"draft": [("readonly", False)]})
