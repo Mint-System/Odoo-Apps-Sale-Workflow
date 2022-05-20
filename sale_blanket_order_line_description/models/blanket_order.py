ff17b7479f9f4211adc2bc33e59b15cd57af5d25
janikvonrotz <contact@janikvonrotz.ch>
2022-05-20 16:58:03 +0200

    [sale_blanket_order_line_description] change type and onchange

--- sale_blanket_order_line_description/models/blanket_order.py
+++ sale_blanket_order_line_description/models/blanket_order.py
@@ -4,8 +4,12 @@ from odoo import api, fields, models, _
 class BlanketOrderLine(models.Model):
     _inherit = "sale.blanket.order.line"
 
-    @api.onchange("product_id")
-    def product_id_change(self):
+    name = fields.Text("Description", tracking=True)
+
+    @api.onchange("product_id", "original_uom_qty")
+    def onchange_product(self):
+        current_name = self.name
+        super().onchange_product()
         if self.product_id.description_sale:
             product = self.product_id.with_context(
                 lang=self.order_id.partner_id.lang,
@@ -13,3 +17,5 @@ class BlanketOrderLine(models.Model):
             self.name = product.description_sale
         elif self.product_id:
             self.name = self.product_id.name
+        if current_name:
+            self.name = current_name
