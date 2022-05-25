d513856f4713d172ccbc62334a020a93389a3da4
janikvonrotz <contact@janikvonrotz.ch>
2022-05-25 17:16:02 +0200

    [sale_blanket_order_contact_person] depend on sale type [sale_order_contact_person] depend on sale type

--- sale_blanket_order_contact_person/models/blanket_order.py
+++ sale_blanket_order_contact_person/models/blanket_order.py
@@ -1,7 +1,17 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class BlanketOrder(models.Model):
     _inherit = "sale.blanket.order"
 
     partner_contact_id = fields.Many2one('res.partner', string="Contact Person", states={"draft": [("readonly", False)]})
+
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        res= super().onchange_partner_id()
+        addr = self.partner_id.address_get(['sale'])
+        values = {
+            'partner_contact_id': addr['sale'],
+        }
+        self.update(values)
+        return res
--- sale_order_contact_person/models/sale_order.py
+++ sale_order_contact_person/models/sale_order.py
@@ -5,3 +5,13 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     partner_contact_id = fields.Many2one('res.partner', string="Contact Person")
+
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        res= super().onchange_partner_id()
+        addr = self.partner_id.address_get(['sale'])
+        values = {
+            'partner_contact_id': addr['sale'],
+        }
+        self.update(values)
+        return res
