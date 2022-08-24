556da3d8036550e60f13381b2af25bf2e5e6aa0a
janikvonrotz <contact@janikvonrotz.ch>
2022-08-24 15:48:58 +0200

    [sale_blanket_order_line_description] add option hide_default_code

--- sale_blanket_order_line_description/__manifest__.py
+++ sale_blanket_order_line_description/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://www.mint-system.ch",
     "category": "Sales",
-    "version": "14.0.1.1.0",
+    "version": "14.0.1.1.1",
     "license": "AGPL-3",
     "depends": ["sale_blanket_order"],
     "data": ["views/view_blanket_order_form.xml"],
