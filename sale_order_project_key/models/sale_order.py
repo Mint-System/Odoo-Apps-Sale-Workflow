9e936f087976bd6e489a8de03a4c49dd2bb9244b
janikvonrotz <contact@janikvonrotz.ch>
2022-11-04 07:40:15 +0100

    [sale_order_project_key] set name in form

--- sale_order_project_key/models/sale_order.py
+++ sale_order_project_key/models/sale_order.py
@@ -6,23 +6,23 @@ _logger = logging.getLogger(__name__)
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    @api.model
-    def create(self, vals):
-        """Use project key as name for sale order."""
+    # @api.model
+    # def create(self, vals):
+    #     """Use project key as name for sale order."""
         
-        if vals.get('project_id'):
-            project = self.env['project.project'].browse(vals.get('project_id'))
-            if project.code:
-                vals['name'] = project.code
-        res = super(SaleOrder, self).create(vals)
+    #     if vals.get('project_id'):
+    #         project = self.env['project.project'].browse(vals.get('project_id'))
+    #         if project.code:
+    #             vals['name'] = project.code
+    #     res = super(SaleOrder, self).create(vals)
 
-        # Update sale order link on project      
-        if res.project_id:
-            res.project_id.write({
-                'partner_id': res.partner_id.id,
-            })
+    #     # Update sale order link on project      
+    #     if res.project_id:
+    #         res.project_id.write({
+    #             'partner_id': res.partner_id.id,
+    #         })
         
-        return res
+    #     return res
 
     @api.onchange('project_id')
     def _onchange_project_id(self):
