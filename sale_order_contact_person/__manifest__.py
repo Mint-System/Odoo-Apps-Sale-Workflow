6e4b7867e09062f35fa372abd369b0b914613f83
janikvonrotz <contact@janikvonrotz.ch>
2022-12-09 09:38:19 +0100

    [sale_order_contact_person] change translation

--- sale_order_contact_person/__manifest__.py
+++ sale_order_contact_person/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://www.mint-system.ch",
     "category": "Sale",
-    "version": "15.0.1.1.1",
+    "version": "15.0.1.2.0",
     "license": "AGPL-3",
     "depends": ["sale", "partner_type_sale"],
     "data": ["views/sale_order.xml"],
--- sale_order_contact_person/models/sale_order.py
+++ sale_order_contact_person/models/sale_order.py
@@ -6,7 +6,7 @@ _logger = logging.getLogger(__name__)
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    partner_contact_id = fields.Many2one('res.partner', string='Contact Person')
+    partner_contact_id = fields.Many2one('res.partner', string='Sale Contact Address')
 
     @api.onchange('partner_id')
     def onchange_partner_id(self):
