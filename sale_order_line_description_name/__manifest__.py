ff13f59c0c135ed2926aa569083df3ae2410b7ac
Mitchel Admin <mitcheladmin@example.com>
2023-02-23 11:38:30 +0100

    [sale_order_line_description_name] new param

--- sale_order_line_description_name/__manifest__.py
+++ sale_order_line_description_name/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://www.mint-system.ch",
     "category": "Sales",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.1.0",
     "license": "AGPL-3",
     "depends": ["sale_order_line_description"],
     "installable": True,
--- sale_order_line_description_name/models/sale_order_line.py
+++ sale_order_line_description_name/models/sale_order_line.py
@@ -1,4 +1,5 @@
 from odoo import api, models
+import ast
 
 
 class SaleOrderLine(models.Model):
@@ -7,8 +8,14 @@ class SaleOrderLine(models.Model):
     @api.onchange("product_id")
     def product_id_change(self):
         res = super(SaleOrderLine, self).product_id_change()
-        if not self.product_id:
-            return res
+        prefix_sale_description = ast.literal_eval(self.env["ir.config_parameter"].sudo().get_param("sale_order_line_description_name.prefix_sale_description", "False"))
+
+        # Set product name without reference as description
         if not self.product_id.description_sale:
             self.name = self.product_id.name
+
+        # Prefix description with product name
+        if self.product_id.description_sale and prefix_sale_description:
+            self.name = self.product_id.name + '\n' + self.name
+
         return res
