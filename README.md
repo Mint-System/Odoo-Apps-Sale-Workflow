f06622137f3b39bea635b96af46f4d60c375a001
Mitchel Admin <mitcheladmin@example.com>
2023-03-01 11:35:20 +0100

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Odoo Apps: Sale Workflow
+
+Collection of sale model related modules.
+
+## Usage
+
+Clone module into Odoo addon directory.
+
+```bash
+git clone git@github.com:mint-system/odoo-apps-sale-workflow.git ./addons/sale_workflow
+```
+
+## Available modules
+
+| Module | Summary |
+| --- | --- |
+| [sale_subscription_disable_tokenization](sale_subscription_disable_tokenization) |         Do not force payment tokenization for order with subscriptions. |
---
+++ sale_subscription_disable_tokenization/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Sale Subscription Disable Tokenization",
+    "summary": """
+        Do not force payment tokenization for order with subscriptions.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Sale",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["sale_subscription"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
