31ab5052d37b36eb45dc211eb336f58b2c6fd1cc
Janik von Rotz <login@janikvonrotz.ch>
2023-05-18 16:10:06 +0200

    [sale_order_line_pricelist_fixed_discount] support min quantity

--- sale_order_line_pricelist_fixed_discount/models/sale_order_line.py
+++ sale_order_line_pricelist_fixed_discount/models/sale_order_line.py
@@ -7,9 +7,9 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
 
-    @api.onchange('product_id', 'price_unit')
+    @api.onchange('product_id', 'price_unit', 'product_uom_qty')
     def _onchange_price_rule(self):
-        '''Filter and apply pricelist rule with fixed discount.'''
+        """Filter and apply pricelist rule with fixed discount."""
 
         # Read filter date from context
         date = self._context.get('date') or self.order_id.commitment_date or self.order_id.date_order
@@ -23,7 +23,7 @@ class SaleOrderLine(models.Model):
         
         if rule_ids:
             
-            # Get pricelist rule filtered by date                
+            # Get pricelist rule filtered by date
             rule_ids_with_date = rule_ids.filtered(lambda l:
                 (l.date_start and l.date_end and (l.date_start <= date <= l.date_end)) or
                 (l.date_start and not l.date_end and l.date_start <= date) or
@@ -34,12 +34,20 @@ class SaleOrderLine(models.Model):
                 (not l.date_start and not l.date_end)
             )
 
-            # Select first rule with date and otherwise first rule without date
-            rule_id = (rule_ids_with_date and rule_ids_with_date[0]) or (rule_ids_without_date and rule_ids_without_date[0])
+            # Select first rule with date, otherwise first rule without date
+            rule_ids = rule_ids_with_date or rule_ids_without_date
 
-            # _logger.warning([rule_id.name, rule_ids_with_date, rule_ids_without_date])
+            # Get pricelist rule filtered by minimum quantity
+            rule_ids_with_min_qty = rule_ids.filtered(lambda l: l.min_quantity and (l.min_quantity <= self.product_uom_qty)).sorted(lambda r: r.min_quantity)[-1]
+
+            # Get rules without minimum quantity
+            rule_ids_without_min_qty = rule_ids.filtered(lambda l: not l.min_quantity)
+
+            # Select first rule with minimum quantity, otherwise first rule without minimum quantity
+            rule_id = (rule_ids_with_min_qty and rule_ids_with_min_qty[0]) or (rule_ids_without_min_qty and rule_ids_without_min_qty[0])
 
             # Apply fixed price discount
             if rule_id.price_discount != 0:
+
                 # self.price_unit = rule_id.fixed_price
                 self.discount = rule_id.price_discount
