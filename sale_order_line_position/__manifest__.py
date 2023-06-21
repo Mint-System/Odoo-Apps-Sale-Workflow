5bd6b09e37fa8e7b3b3b8741988aa3224e41a0b3
Janik von Rotz <login@janikvonrotz.ch>
2023-06-21 09:35:03 +0200

    [sale_order_line_position] add widget

--- sale_order_line_position/__manifest__.py
+++ sale_order_line_position/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://www.mint-system.ch",
     "category": "Sale",
-    "version": "15.0.1.1.0",
+    "version": "15.0.1.1.1",
     "license": "AGPL-3",
     "depends": ["sale"],
     "data": ["views/sale.xml", "views/product_template.xml"],
