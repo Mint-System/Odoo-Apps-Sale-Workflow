0e7ccaf8a0b6cadabac4cb340927a5ace3ec5105
Janik von Rotz <login@janikvonrotz.ch>
2023-10-16 15:21:26 +0200

    sale_subscription_period_discount: sort

--- sale_subscription_period_discount/models/sale_order_recurrence.py
+++ sale_subscription_period_discount/models/sale_order_recurrence.py
@@ -1,7 +1,7 @@
 import datetime
 import logging
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 _logger = logging.getLogger(__name__)
 
@@ -19,12 +19,11 @@ class SaleOrderRecurrence(models.Model):
 
 class SaleOrderPeriodDiscount(models.Model):
     _name = "sale.temporal.period_discount"
-    _desciption = "Period Discount"
-    _order = "sequence"
+    _description = "Period Discount"
+    _order = "from_date desc"
 
-    sequence = fields.Integer()
     name = fields.Char(compute="_compute_name")
-    from_date = fields.Date(compute="_compute_from_date")
+    from_date = fields.Date(compute="_compute_from_date", store=True)
     recurrence_id = fields.Many2one("sale.temporal.recurrence", required=True)
     day = fields.Integer(default=1, required=True)
     month = fields.Selection(
@@ -47,6 +46,7 @@ class SaleOrderPeriodDiscount(models.Model):
     )
     discount = fields.Float(string="Discount (%)", digits="Discount", required=True)
 
+    @api.depends("day", "month")
     def _compute_from_date(self):
         for discount in self:
             discount.from_date = datetime.date(
