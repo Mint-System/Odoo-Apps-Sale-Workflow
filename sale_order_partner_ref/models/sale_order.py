ae78bb3a5299f5c6ec2640da75ecd02bd597e7ee
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-01-30 09:04:44 +0100

    feat: add translation

--- sale_order_partner_ref/models/sale_order.py
+++ sale_order_partner_ref/models/sale_order.py
@@ -8,4 +8,4 @@ _logger = logging.getLogger(__name__)
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    partner_ref = fields.Char(related="partner_id.ref")
+    partner_ref = fields.Char(related="partner_id.ref", string="Customer No.")
