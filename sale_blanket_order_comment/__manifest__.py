d1a1d3b32c7923f8c87980c28c4dc433910af9b7
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-03-07 15:27:52 +0100

    feat: migrate sale_blanket_order_comment

---
+++ sale_blanket_order_comment/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Sale Blanket Order Comment",
+    "summary": """
+        Comment field for sale blanket order.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Sale",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["sale_blanket_order", "sale_order_comment"],
+    "data": ["views/sale_blanket_order.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
---
+++ sale_blanket_order_comment/models/blanket_order.py
@@ -0,0 +1,8 @@
+from xml.etree.ElementTree import Comment
+from odoo import api, fields, models, _
+
+
+class BlanketOrder(models.Model):
+    _inherit = 'sale.blanket.order'
+
+    comment = fields.Text(tracking=True)
