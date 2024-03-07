afcff4529bd923666121a387fbe22ba167de2812
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-03-07 15:52:49 +0100

    feat: migrate sale_order_contact_person

---
+++ sale_order_contact_person/models/sale_order.py
@@ -0,0 +1,26 @@
+from odoo import api, fields, models
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    partner_contact_id = fields.Many2one('res.partner', string="Contact Person")
+
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        res= super().onchange_partner_id()
+        addr = self.partner_id.address_get(['sale'])
+        values = {
+            'partner_contact_id': addr['sale'],
+        }
+        self.update(values)
+        return res
+
+    def _create_invoices(self, grouped=False, final=False, date=None):
+        moves = super()._create_invoices(grouped=grouped, final=final, date=date)
+        moves.write({
+            'partner_sale_id': self.partner_contact_id.id
+        })
+        return moves
