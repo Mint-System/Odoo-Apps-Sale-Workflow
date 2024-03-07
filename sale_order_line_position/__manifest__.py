e99c64fffbef9c6c543d69bf902643269f437fd6
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-03-07 10:46:40 +0100

    feat: migrate module to 16.0

---
+++ sale_order_line_position/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Sale Order Line Position",
+    "summary": """
+        Show position numbers on sale order lines.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Sale",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "data": ["views/sale.xml", "views/product_template.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
