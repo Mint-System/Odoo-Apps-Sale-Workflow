739a28a5e727ecf5694570a765a631b73dc4ea2c
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-03-11 14:27:12 +0100

    feat: migrate sale_blanket_order_readonly_states

---
+++ sale_blanket_order_readonly_states/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Sale Blanket Order Readonly States",
+    "summary": """
+        Override readonly states.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Sales",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["sale_blanket_order_cancel_state",],
+    "data": ["views/view_blanket_order_form.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
