11a32a2778686d30d79c37197d62501317ffb658
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-03-11 14:26:14 +0100

    feat: migrate sale_blanket_order_send

---
+++ sale_blanket_order_send/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Sale Blanket Order Send",
+    "summary": """
+        Send blanket order by e-mail.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Sale",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["sale_blanket_order_date_confirmed"],
+    "data": ["data/mail_data.xml", "views/sale_blanket_order.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
