d8f767a0b061821f866dc775db92ddbdc17e980b
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-03-12 10:53:23 +0100

    feat: migrate sale_order_template_notes

---
+++ sale_order_template_notes/models/sale_order.py
@@ -0,0 +1,16 @@
+from odoo import api, fields, models, _
+import logging
+_logger = logging.getLogger(__name__)
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.onchange('sale_order_template_id')
+    def _onchange_sale_order_template_id(self):
+        res = super(SaleOrder, self)._onchange_sale_order_template_id()
+        # if notes are set in template overwrite the order notes
+        if self.sale_order_template_id.note_header:
+            self.note_header = self.sale_order_template_id.note_header
+        if self.sale_order_template_id.note_footer:
+            self.note_footer = self.sale_order_template_id.note_footer
+        return res
---
+++ sale_order_template_notes/models/sale_order_template.py
@@ -0,0 +1,7 @@
+from odoo import api, fields, models, _
+
+class SaleOrderTemplate(models.Model):
+    _inherit = "sale.order.template"
+
+    note_header = fields.Html(translate=False)
+    note_footer = fields.Html(translate=False)
