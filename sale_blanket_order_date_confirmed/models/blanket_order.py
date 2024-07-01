d9b5f3e23e4dd0d16727b132db47680170f990a7
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-07-01 15:51:51 +0200

    fix: revert changes

--- sale_blanket_order_date_confirmed/models/blanket_order.py
+++ sale_blanket_order_date_confirmed/models/blanket_order.py
@@ -5,9 +5,11 @@ class BlanketOrder(models.Model):
     _inherit = "sale.blanket.order"
 
     date_confirmed = fields.Date(
+        string="Confirmation Date",
         readonly=True,
         index=True,
         copy=False,
+        help="Confirmation date of blanket order.",
     )
 
     def action_confirm(self):
