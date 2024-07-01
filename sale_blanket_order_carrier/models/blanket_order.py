35d47df43b544d833d2df2f58304ed3739899eac
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-07-01 10:52:52 +0200

    feat: refactor the model, add test instructions

--- sale_blanket_order_carrier/models/blanket_order.py
+++ sale_blanket_order_carrier/models/blanket_order.py
@@ -6,12 +6,13 @@ class BlanketOrder(models.Model):
 
     carrier_id = fields.Many2one(
         "delivery.carrier",
-        string="Delivery Method",
         domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
-        help="Fill this field if you plan to invoice the shipping based on picking.",
+        compute="_compute_carrier_id",
+        store=True,
     )
 
-    @api.onchange("partner_id")
-    def _onchange_carrier_id(self):
-        for order in self.filtered(lambda bo: not bo.carrier_id):
-            order.carrier_id = order.partner_id.property_delivery_carrier_id
+    @api.depends("partner_id")
+    def _compute_carrier_id(self):
+        for order in self:
+            if not order.carrier_id:
+                order.carrier_id = order.partner_id.property_delivery_carrier_id
