ed085ec043dada854f86e848b2c1052d9e0c3530
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-07-01 14:19:53 +0200

    feat: format readme, add test instructions, lint module

--- sale_blanket_order_date_confirmed/models/blanket_order.py
+++ sale_blanket_order_date_confirmed/models/blanket_order.py
@@ -5,11 +5,9 @@ class BlanketOrder(models.Model):
     _inherit = "sale.blanket.order"
 
     date_confirmed = fields.Date(
-        string="Confirmation Date",
         readonly=True,
         index=True,
         copy=False,
-        help="Confirmation date of blanket order.",
     )
 
     def action_confirm(self):
