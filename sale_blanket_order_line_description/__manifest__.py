880f93682fc28e920b620618687c767fe888104d
Laszlo Krajnikovszkij <laszlo@laszlo.is>
2024-08-29 12:01:38 +0200

    feat: restore the onchange method

--- sale_blanket_order_line_description/__manifest__.py
+++ sale_blanket_order_line_description/__manifest__.py
@@ -9,7 +9,6 @@
     "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["sale_blanket_order"],
-    "data": ["views/view_blanket_order_form.xml"],
     "installable": True,
     "application": False,
     "auto_install": False,
