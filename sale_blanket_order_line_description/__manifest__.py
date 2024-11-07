98c46fc6437c05dbe317da25531194fbef2cfa89
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-11-07 14:14:56 +0100

    feat: migrate module

---
+++ sale_blanket_order_line_description/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Sale Blanket Order Line Description",
+    "summary": """
+        Copy description field of order lines to sale order.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Sales",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["sale_blanket_order"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
