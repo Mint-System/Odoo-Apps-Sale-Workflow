f1753f00f71d387b150054b6c37d84d4b3935f5e
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-11-07 16:51:08 +0100

    feat: init module upgrade

---
+++ sale_order_line_purchase_margin/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Sale Order Line Purchase Margin",
+    "summary": """
+        Calculate sale line margin from linked purchase.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Sales",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["sale_margin", "sale_purchase_stock"],
+    "data": ["views/sale.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
