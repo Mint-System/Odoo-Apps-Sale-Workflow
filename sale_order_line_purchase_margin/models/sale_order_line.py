8afd4e243f6cd3631a26237620eb687d05a19730
Ulrich Kilian <uk@science-and-more.de>
2025-04-14 11:09:14 +0200

    fix(sale_order_line_purchase_margin): linting

--- sale_order_line_purchase_margin/models/sale_order_line.py
+++ sale_order_line_purchase_margin/models/sale_order_line.py
@@ -1,13 +1,17 @@
-from statistics import mean
-from odoo import api, models, fields
 import logging
+from statistics import mean
+
+from odoo import fields, models
+
 _logger = logging.getLogger(__name__)
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
-    stock_purchase_line_ids = fields.Many2many('purchase.order.line', compute='_compute_stock_purchase_lines', store=False)
+    stock_purchase_line_ids = fields.Many2many(
+        "purchase.order.line", compute="_compute_stock_purchase_lines", store=False
+    )
 
     def _compute_stock_purchase_lines(self):
         """Get average price from linked purchase order lines."""
@@ -15,7 +19,11 @@ class SaleOrderLine(models.Model):
             stock_purchase_line_ids = []
             purchase_ids = line.order_id._get_purchase_orders()
             if purchase_ids:
-                stock_purchase_line_ids = purchase_ids.order_line.filtered(lambda l: l.product_id == line.product_id)
+                stock_purchase_line_ids = purchase_ids.order_line.filtered(
+                    lambda l: l.product_id == line.product_id
+                )
                 if stock_purchase_line_ids:
-                    line.purchase_price = mean(stock_purchase_line_ids.mapped('price_unit'))
+                    line.purchase_price = mean(
+                        stock_purchase_line_ids.mapped("price_unit")
+                    )
             line.stock_purchase_line_ids = stock_purchase_line_ids
