482640067095c5a8126a19aee29c0dc4cca07f8a
Janik von Rotz <login@janikvonrotz.ch>
2025-06-27 17:00:02 +0200

    fix: module

--- sale_helm/__manifest__.py
+++ sale_helm/__manifest__.py
@@ -5,7 +5,7 @@
         Sell services and assign hosting provider and consulting partner.
     """,
     "author": "Mint System GmbH",
-    "website": "https://www.mint-system.ch",
+    "website": "https://www.mint-system.ch/",
     "category": "Repository",
     "version": "18.0.1.0.0",
     "license": "OPL-1",
