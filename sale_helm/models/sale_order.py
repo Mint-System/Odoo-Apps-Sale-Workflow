61a64ac6b845e61542b52fa15be82fd34d8049e9
Janik von Rotz <login@janikvonrotz.ch>
2025-07-07 20:27:10 +0200

    feat(sale_helm): implement release on confirm

--- sale_helm/models/sale_order.py
+++ sale_helm/models/sale_order.py
@@ -1,6 +1,7 @@
 import logging
 
 from odoo import fields, models
+from odoo.exceptions import ValidationError
 
 _logger = logging.getLogger(__name__)
 
@@ -8,12 +9,30 @@ _logger = logging.getLogger(__name__)
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    chart_ids = fields.One2many("helm.chart", compute="_compute_chart_ids")
-    project_name = fields.Char()
+    project_name = fields.Char(inverse="_inverse_project_name")
     domain = fields.Char()
     consulting_partner_id = fields.Many2one("res.partner")
     cluster_id = fields.Many2one("kubectl.cluster")
 
+    chart_ids = fields.One2many("helm.chart", compute="_compute_chart_ids")
+
     def _compute_chart_ids(self):
         for rec in self:
-            rec.chart_ids = rec.order_line.product_id.chart_ids
+            rec.chart_ids = rec.order_line.product_id.chart_id
+
+    def _inverse_project_name(self):
+        """
+        Ensure project name is alphanumerical.
+        """
+        for rec in self:
+            if rec.project_name:
+                if not rec.project_name.isalnum():
+                    raise ValidationError(_("Project name must only contain alphanumeric characters."))
+
+    def action_confirm(self):
+        for order in self.filtered("chart_ids"):
+            namespace_id = self.env["kubectl.namespace"].create(
+                {"name": order.project_name, "cluster_id": order.cluster_id.id}
+            )
+            order.order_line.filtered("product_id.chart_id")._install_chart(namespace_id)
+        return super().action_confirm()
--- sale_helm/models/sale_order_line.py
+++ sale_helm/models/sale_order_line.py
@@ -9,3 +9,19 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     release_id = fields.Many2one("helm.release")
+
+    def _install_chart(self, namespace_id):
+        """
+        Create and install chart release.
+        """
+        self.ensure_one()
+        chart_id = self.product_id.chart_id
+        release_id = self.release_id.create(
+            {
+                "name": self.order_id.project_name,
+                "chart_id": chart_id.id,
+                "context_id": self.order_id.cluster_id.context_ids[0].id,
+                "namespace_id": namespace_id.id,
+                "partner_id": self.order_partner_id.id,
+            }
+        )
