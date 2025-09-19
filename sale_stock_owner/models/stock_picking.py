3314c2cef6cc6d8c69658c3078f4be3771b877d4
Ulrich Kilian <uk@science-and-more.de>
2025-09-19 11:06:04 +0200

    feat(sale_stock_owner): set stock owner to  picking owner

--- sale_stock_owner/models/stock_picking.py
+++ sale_stock_owner/models/stock_picking.py
@@ -23,3 +23,20 @@ class StockPicking(models.Model):
                     _logger.warning(["Set owner_id for:", stock_quants])
                     # stock_quants.write({"owner_id": picking.owner_id.id})
         return res
+
+
+    def _action_done(self):
+        res = super()._action_done()
+        for picking in self:
+            if picking.owner_id:
+                for line in picking.move_line_ids:
+                    stock_quants = self.env["stock.quant"].search(
+                        [
+                            ("product_id", "=", line.product_id.id),
+                            ("lot_id", "=", line.lot_id.id),
+                            ("location_id", "=", picking.location_id.id),
+                        ]
+                    )
+                    stock_quants.write({"owner_id": picking.owner_id.id})
+        
+        return res
