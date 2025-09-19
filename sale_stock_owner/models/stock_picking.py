209fe4035f4d25860dc81309f14341f050bd4411
Ulrich Kilian <uk@science-and-more.de>
2025-09-19 14:56:34 +0200

    feat(sale_stock_owner): linting

--- sale_stock_owner/models/stock_picking.py
+++ sale_stock_owner/models/stock_picking.py
@@ -8,23 +8,6 @@ _logger = logging.getLogger(__name__)
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    def button_validate(self):
-        res = super().button_validate()
-        for picking in self:
-            if picking.owner_id:
-                for line in picking.move_line_ids:
-                    stock_quants = self.env["stock.quant"].search(
-                        [
-                            ("product_id", "=", line.product_id.id),
-                            ("lot_id", "=", line.lot_id.id),
-                            ("location_id", "=", picking.location_id.id),
-                        ]
-                    )
-                    _logger.warning(["Set owner_id for:", stock_quants])
-                    # stock_quants.write({"owner_id": picking.owner_id.id})
-        return res
-
-
     def _action_done(self):
         res = super()._action_done()
         for picking in self:
@@ -38,5 +21,5 @@ class StockPicking(models.Model):
                         ]
                     )
                     stock_quants.write({"owner_id": picking.owner_id.id})
-        
+
         return res
