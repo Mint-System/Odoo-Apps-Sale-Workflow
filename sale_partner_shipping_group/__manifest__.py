bc629537feb283e6fe35cb9329f08fe0b36a5a77
Janik von Rotz <login@janikvonrotz.ch>
2025-10-24 14:51:18 +0200

    feat(sale_partner_shipping_group): add module for shipping address access control on sale orders and invoices

---
+++ sale_partner_shipping_group/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Sale Partner Shipping Group",
+    "summary": """
+        Access group for shipping address on sale orders and invoices.
+    """,
+    "author": "Mint System GmbH",
+    "website": "https://www.mint-system.ch/",
+    "category": "Technical",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["sale", "account"],
+    "data": ["security/security.xml", "views/account_move.xml", "views/sale_order.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
