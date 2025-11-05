2ca2f50a3e924822836366526bc807e5aa0bad14
Ulrich Kilian <uk@science-and-more.de>
2025-11-05 13:39:56 +0100

    feat(sale_stock_owner): refactoring

--- sale_stock_owner/models/stock_picking.py
+++ sale_stock_owner/models/stock_picking.py
@@ -8,36 +8,39 @@ _logger = logging.getLogger(__name__)
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
+    def _set_or_remove_owner(self, location_id, action='set'):
+        picking = self
+        for line in picking.move_line_ids:
+            _logger.warning(f"########## product: {line.product_id.name}, lot: {line.lot_id.name}, loc: {picking.location_id.name}, dest loc {picking.location_dest_id.name}")
+            loc_id = picking.location_dest_id.id if picking.picking_type_id.name == 'Returns' else picking.location_id.id
+            stock_quants = self.env["stock.quant"].search(
+                [
+                    ("product_id", "=", line.product_id.id),
+                    ("lot_id", "=", line.lot_id.id),
+                    ("location_id", "=", location_id),
+                ]
+            )
+            _logger.warning(f"stock quants: {stock_quants}")
+            if action == 'remove':
+                stock_quants.write({"owner_id": False})
+            elif action == 'set':
+                stock_quants.write({"owner_id": picking.owner_id.id})
+
+
     def _action_done(self):
         res = super()._action_done()
         for picking in self:
             _logger.warning(f"########## picking owner: {picking.owner_id}, picking type: {picking.picking_type_id}, {picking.picking_type_id.name}, {picking.picking_type_code}")
-            # alternative 1: add owner to stock
-            if (picking.owner_id and picking.location_id.clear_owner) or picking.picking_type_id.name == 'Returns':
-                _logger.warning("###### remove owner")
+            
+            if picking.picking_type_id.name == 'Returns':
+                loc_id = picking.location_dest_id.id if picking.picking_type_id.name == 'Returns'
+                picking._set_or_remove_owner(loc_id)
+            elif (picking.owner_id and picking.location_id.clear_owner):
                 picking.write({"owner_id": False})
-                for line in picking.move_line_ids:
-                    _logger.warning(f"########## product: {line.product_id.name}, lot: {line.lot_id.name}, loc: {picking.location_id.name}, dest loc {picking.location_dest_id.name}")
-                    loc_id = picking.location_dest_id.id if picking.picking_type_id.name == 'Returns' else picking.location_id.id
-                    stock_quants = self.env["stock.quant"].search(
-                        [
-                            ("product_id", "=", line.product_id.id),
-                            ("lot_id", "=", line.lot_id.id),
-                            ("location_id", "=", loc_id),
-                        ]
-                    )
-                    _logger.warning(f"stock quants: {stock_quants}")
-                    stock_quants.write({"owner_id": False})
-
-            if picking.owner_id and not picking.location_id.clear_owner:
-                for line in picking.move_line_ids:
-                    stock_quants = self.env["stock.quant"].search(
-                        [
-                            ("product_id", "=", line.product_id.id),
-                            ("lot_id", "=", line.lot_id.id),
-                            ("location_id", "=", picking.location_id.id),
-                        ]
-                    )
-                    stock_quants.write({"owner_id": picking.owner_id.id})
+                loc_id = picking.location_id.id
+                picking._set_or_remove_owner(locvation_id=loc_id, action='remove')
+            elif picking.owner_id and not picking.location_id.clear_owner:
+                loc_id = picking.location_id.id
+                picking._set_or_remove_owner(location_id=loc_id, action='set')
 
         return res
