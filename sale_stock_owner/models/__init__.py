ae84c53262e1c07782430e8eb3997cce8d9b62a6
Ulrich Kilian <uk@science-and-more.de>
2025-11-05 11:08:49 +0100

    feat(sale_stock_owner): remove owner from returns

--- sale_stock_owner/models/__init__.py
+++ sale_stock_owner/models/__init__.py
@@ -2,3 +2,4 @@ from . import stock_quant
 from . import sale_order
 from . import stock_location
 from . import stock_picking
+from . import stock_picking_return
--- sale_stock_owner/models/stock_picking.py
+++ sale_stock_owner/models/stock_picking.py
@@ -11,7 +11,25 @@ class StockPicking(models.Model):
     def _action_done(self):
         res = super()._action_done()
         for picking in self:
-            if picking.owner_id:
+            _logger.warning(f"########## picking owner: {picking.owner_id}, picking type: {picking.picking_type_id}, {picking.picking_type_id.name}, {picking.picking_type_code}")
+            # alternative 1: add owner to stock
+            if (picking.owner_id and picking.location_id.clear_owner) or picking.picking_type_id.name == 'Returns':
+                _logger.warning("###### remove owner")
+                picking.write({"owner_id": False})
+                for line in picking.move_line_ids:
+                    _logger.warning(f"########## product: {line.product_id.name}, lot: {line.lot_id.name}, loc: {picking.location_id.name}, dest loc {picking.location_dest_id.name}")
+                    loc_id = picking.location_dest_id.id if picking.picking_type_id.name == 'Returns' else picking.location_id.id
+                    stock_quants = self.env["stock.quant"].search(
+                        [
+                            ("product_id", "=", line.product_id.id),
+                            ("lot_id", "=", line.lot_id.id),
+                            ("location_id", "=", loc_id),
+                        ]
+                    )
+                    _logger.warning(f"stock quants: {stock_quants}")
+                    stock_quants.write({"owner_id": False})
+
+            if picking.owner_id and not picking.location_id.clear_owner:
                 for line in picking.move_line_ids:
                     stock_quants = self.env["stock.quant"].search(
                         [
