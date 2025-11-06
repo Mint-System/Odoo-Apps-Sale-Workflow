4cc280dcafad6b1cb610f6f5d3671d1863963dc0
Ulrich Kilian <uk@science-and-more.de>
2025-11-06 17:37:11 +0100

    feat(sale_stock_owner): add clear owner flag to view

--- sale_stock_owner/models/stock_picking_return.py
+++ sale_stock_owner/models/stock_picking_return.py
@@ -1,17 +1,14 @@
 from odoo import models
 
 
-
-
 class ReturnPicking(models.TransientModel):
-    _inherit = 'stock.return.picking'
+    _inherit = "stock.return.picking"
 
     def _create_returns(self):
         new_picking_id, pick_type_id = super()._create_returns()
 
-        new_picking = self.env['stock.picking'].browse(new_picking_id)
+        new_picking = self.env["stock.picking"].browse(new_picking_id)
 
         new_picking.owner_id = False
 
         return new_picking_id, pick_type_id
-
