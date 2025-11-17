a3c12b737d42350b26c7a6d3c56fffb35ffff18b
Janik von Rotz <login@janikvonrotz.ch>
2025-11-17 11:24:01 +0100

    feat(sale_project_link): make project field editable in sale orders

--- sale_project_link/models/sale_order.py
+++ sale_project_link/models/sale_order.py
@@ -2,7 +2,7 @@
 
 import logging
 
-from odoo import models
+from odoo import fields, models
 
 _logger = logging.getLogger(__name__)
 
@@ -11,4 +11,4 @@ class SaleOrder(models.Model):
     _inherit = ["sale.order"]
 
     # Make project field editable
-    project_id = fields.Many2one('project.project', readonly=False, states={})
+    project_id = fields.Many2one("project.project", readonly=False, states={})
