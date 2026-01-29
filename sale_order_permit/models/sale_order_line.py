c2a2e3cc215176ea6129134bfa92ebb0682cdb58
Ulrich Kilian <uk@sdcience-and-more.de>
2026-01-29 23:20:35 +0100

    feat(sale_order_permit): calculation of week permits duration

--- sale_order_permit/models/sale_order_line.py
+++ sale_order_permit/models/sale_order_line.py
@@ -1,7 +1,7 @@
 import logging
-from datetime import datetime, date, timedelta
+from datetime import date, datetime, timedelta
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
 
@@ -12,23 +12,21 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     date_from = fields.Date(
-        string='Start Date', required=True,
-        help='Start Date of Patent',
-        default=fields.Date.today()
-        )
+        string="Start Date",
+        required=True,
+        help="Start Date of Patent",
+        default=fields.Date.today(),
+    )
 
-    date_to = fields.Date(
-        compute="_compute_date_to")
+    date_to = fields.Date(compute="_compute_date_to")
 
-
-
-    @api.onchange('product_id')
+    @api.onchange("product_id")
     def _onchange_product_id_date_from(self):
         for line in self:
             if not line.product_id:
                 continue
 
-            if line.product_id.duration == 'year':
+            if line.product_id.duration == "year":
                 today = fields.Date.context_today(line)
                 october_1 = date(today.year, 10, 1)
 
@@ -37,23 +35,76 @@ class SaleOrderLine(models.Model):
             else:
                 line.date_from = fields.Datetime.now()
 
+    # @api.depends("date_from")
+    # def _compute_date_to(self):
+    #     duration_map = {
+    #         "year": {"days": 365},
+    #         "day": {"days": 1},
+    #         "week": {"weeks": 1},
+    #     }
+    #     for line in self:
+    #         if not line.date_from or not line.product_id:
+    #             line.date_to = line.date_from
+    #             continue
 
+    #         duration = line.product_id.duration
 
-    @api.depends("date_from")
+    @api.depends("date_from", "product_id.duration")
     def _compute_date_to(self):
-        duration_map = {
-            "year": {"days": 365},
-            "day": {"days": 1},
-            "week": {"weeks": 1},
-        }
         for line in self:
+            if not line.date_from or not line.product_id:
+                line.date_to = line.date_from
+                continue
+
             duration = line.product_id.duration
-            _logger.warning(f"duration: {duration}")
-            if line.date_from and line.product_id.duration in duration_map.keys():
-               line.date_to = line.date_from + timedelta(**duration_map[duration])
+
+            if duration == "day":
+                line.date_to = line.date_from + timedelta(days=1)
+                continue
+
+            if duration == "year":
+                line.date_to = line.date_from + timedelta(days=365)
+                continue
+
+            if duration == "week":
+                current_date = line.date_from
+                today = fields.Date.today()
+
+                valid_days = 0
+                country = "Schweiz"
+
+                calendars = (
+                    self.env["calendar.public.holiday"]
+                    .sudo()
+                    .search(
+                        [
+                            ("year", ">=", today.year),
+                            ("country_id.name", "=", country),
+                        ]
+                    )
+                )
+
+                public_holidays = calendars.mapped("line_ids.date")
+
+                while valid_days < 7:
+                    current_date += timedelta(days=1)
+
+                    # Skip Sundays
+                    if current_date.weekday() == 6:
+                        continue
+
+                    # Skip public holidays
+                    if current_date in public_holidays:
+                        continue
+
+                    valid_days += 1
+
+                line.date_to = current_date - timedelta(days=1)
             else:
                 line.date_to = line.date_from
 
-
-
-
+            # _logger.warning(f"duration: {duration}")
+            # if line.date_from and line.product_id.duration in duration_map.keys():
+            #    line.date_to = line.date_from + timedelta(**duration_map[duration])
+            # else:
+            #     line.date_to = line.date_from
