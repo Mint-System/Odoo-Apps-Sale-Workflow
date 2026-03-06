7b7082936aee9db6157cd5d7e32e2c8cc1bd8819
Janik von Rotz <login@janikvonrotz.ch>
2026-03-06 18:05:09 +0100

    chore(sale_timesheet_billable_price, sale_timesheet_report_group_invoice_type, sale_timesheet_report_project_details): update version to 16.0.1.0.0 and fix template formatting in sale_timesheet_estimate_report

--- sale_timesheet_report_project_details/__manifest__.py
+++ sale_timesheet_report_project_details/__manifest__.py
@@ -8,7 +8,7 @@
     "website": "https://www.mint-system.ch/",
     "category": "Repository",
     "development_status": "Production/Stable",
-    "version": ".1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["sale_timesheet"],
     "data": [
