191c74c029719be43e3a65def52ee4a1d609ac5c
Ulrich Kilian <uk@sdcience-and-more.de>
2026-03-11 20:13:05 +0100

    feat(sale_order_permit): add category to session in product route of controller

--- sale_order_permit/controllers/main.py
+++ sale_order_permit/controllers/main.py
@@ -1,13 +1,29 @@
+import logging
+
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo import http
+
+_logger = logging.getLogger(__name__)
 
 class WebsiteSaleCustom(WebsiteSale):
 
     @http.route()
     def shop(self, page=0, category=None, search='', min_price=0.0, max_price=0.0, ppg=False, **post):
+        # here category is category object
         if category:
             http.request.session['public_category_id'] = category.id
         else:
             http.request.session.pop('public_category_id', None)
 
         return super().shop(page=page, category=category, search=search, min_price=min_price, max_price=max_price, ppg=ppg, **post)
+
+
+    @http.route()
+    def product(self, product, category='', search='', **kwargs):
+        # here category is category id
+        if category:
+            http.request.session['public_category_id'] = category
+        else:
+            http.request.session.pop('public_category_id', None)
+
+        return super().product(product, category, search, **kwargs)
