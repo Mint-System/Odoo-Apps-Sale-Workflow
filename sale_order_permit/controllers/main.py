5311433eb96b91e0175f3340cad9fcbd4aa3f782
Ulrich Kilian <uk@sdcience-and-more.de>
2026-03-17 10:08:18 +0100

    feat(sale_order_permit): working solution for reset pricelist if product changes

--- sale_order_permit/controllers/main.py
+++ sale_order_permit/controllers/main.py
@@ -1,6 +1,7 @@
 import logging
 
 from odoo import http
+from odoo.http import request
 
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
@@ -8,29 +9,20 @@ _logger = logging.getLogger(__name__)
 
 
 class WebsiteSaleCustom(WebsiteSale):
-    # @http.route()
-    # def product(self, product, category='', search='', **kwargs):
-    #     public_category_ids = product.public_categ_ids.ids
-    #     if public_category_ids:
-    #         http.request.session['product_public_category_ids'] = public_category_ids
-    #     else:
-    #         http.request.session.pop('product_public_category_ids', None)
-
-    #     if category:
-    #         http.request.session['public_category_id'] = category
-    #     else:
-    #         http.request.session.pop('public_category_id', None)
-
-    #     return super().product(product, category, search, **kwargs)
-
     @http.route()
     def product(self, product, category="", search="", **kwargs):
-        # Get dummy pricelist (sequence = 0)
-        dummy_pricelist = http.request.env["product.pricelist"].search([("sequence", "=", 0)], limit=1)
+        last_product_id = request.session.get('last_viewed_product_id')
+        current_product_id = product.id
+        
+       
+        if last_product_id != current_product_id:
+            http.request.session["force_dummy_pricelist"] = True
+        
+        request.session['last_viewed_product_id'] = product.id
 
-        # Reset pricelist in session to dummy
-        if dummy_pricelist:
-            http.request.session["pricelist"] = dummy_pricelist.id
-
-        # Call parent method
         return super().product(product, category, search, **kwargs)
+
+
+
+
+
--- sale_order_permit/models/website.py
+++ sale_order_permit/models/website.py
@@ -1,6 +1,7 @@
 import logging
 
 from odoo import models
+from odoo.http import request
 
 _logger = logging.getLogger(__name__)
 
@@ -8,32 +9,19 @@ _logger = logging.getLogger(__name__)
 class Website(models.Model):
     _inherit = "website"
 
-    # def _get_current_pricelist(self):
-    #     pricelist = super()._get_current_pricelist()
 
-    #     # domain filtered = category matching
-    #     available_pricelists = self.get_pricelist_available()
+    def _get_current_pricelist(self):
+        pricelist = super()._get_current_pricelist()
 
-    #     # If pricelist is not in filtered list, fall back to first available
-    #     if pricelist and pricelist not in available_pricelists:
-    #         if available_pricelists:
-    #             pricelist = available_pricelists[0]
-    #             # Update session to persist selection
-    #             if hasattr(self.env, 'request') and self.env.request.session:
-    #                 self.env.request.session['website_sale_current_pl'] = pricelist.id
-    #             _logger.info("Fallback: current pricelist %s not in filtered list, using %s", pricelist.id, available_pricelists[0].id)
+        if request.session.get("force_dummy_pricelist"):
+            request.session.pop("force_dummy_pricelist", None)
 
-    #     return pricelist
+            return request.env["product.pricelist"].search(
+                [("sequence", "=", 0)], limit=1
+            )
 
-    # def _compute_pricelist_id(self):
-    #     for website in self:
-    #         # Get actual current pricelist (via _get_current_pricelist)
-    #         pricelist = website._get_current_pricelist()
-    #         _logger.warning(f"pricelist: {pricelist}")
-    #         _logger.warning(f"request.session.get('website_sale_current_pl'): {request.session.get('website_sale_current_pl')}")
+        return pricelist
 
-    #         # If no pricelist is explicitly selected (session empty), return False for UI
-    #         if not request.session.get('website_sale_current_pl'):
-    #             website.pricelist_id = False  # Show "Select Price List"
-    #         else:
-    #             website.pricelist_id = pricelist  # Show actual pricelist name
+
+
+
