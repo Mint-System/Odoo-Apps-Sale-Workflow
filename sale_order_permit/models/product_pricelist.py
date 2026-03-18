be7b10a51a8f46cc765f7cfab379425c339bf5db
Ulrich Kilian <uk@sdcience-and-more.de>
2026-03-18 09:53:33 +0100

    feat(sale_order_permit): remove is_dummy field on pricelist

--- sale_order_permit/models/product_pricelist.py
+++ sale_order_permit/models/product_pricelist.py
@@ -11,20 +11,3 @@ class ProductPricelist(models.Model):
     is_dummy = fields.Boolean(help="Used as a placeholder. Products are hidden until another pricelist is selected.")
 
     public_category_ids = fields.Many2many("product.public.category", string="Website categories")
-
-    # def _get_website_pricelists_domain(self, website):
-    #     domain = super()._get_website_pricelists_domain(website)
-
-    #     # get selected category from session injected in main controller
-    #     product_category_ids = http.request.session.get('product_public_category_ids')
-    #     _logger.warning(f"product_category_ids: {product_category_ids}")
-
-    #     if product_category_ids:
-    #         # Filter: pricelist must have at least one of these categories, OR no categories assigned
-    #         domain += [
-    #             '|',
-    #             ('public_category_ids', 'in', product_category_ids),
-    #             ('public_category_ids', '=', False),
-    #         ]
-
-    #     return domain
