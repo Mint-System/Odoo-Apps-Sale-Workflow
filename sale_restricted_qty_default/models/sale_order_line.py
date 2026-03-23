f5a625dd6fb37d0f78ec8d1b0c8c9e6ec2f19319
Ulrich Kilian <uk@sdcience-and-more.de>
2026-03-23 16:00:41 +0100

    feat(sale_restricted_qty_default): allow sale group users to ignore minimum quantity

--- sale_restricted_qty_default/models/sale_order_line.py
+++ sale_restricted_qty_default/models/sale_order_line.py
@@ -13,7 +13,13 @@ class SaleOrderLine(models.Model):
     )
 
     def _inverse_product_uom_qty(self):
+        _logger.warning(f"#### user: {self.env.user}")
         for rec in self:
             product = rec.product_id.product_tmpl_id
-            if product and product.is_sale_own_min_qty_set and product.sale_min_qty > rec.product_uom_qty:
+            if (
+                 product 
+                 and product.is_sale_own_min_qty_set
+                 and product.sale_min_qty > rec.product_uom_qty 
+                 and not self.env.user.has_group('sales_team.group_sale_salesman')
+               ):
                 rec.product_uom_qty = product.sale_min_qty
