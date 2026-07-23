1c5326fd0a061604d664364afe972560b8b54f34
Ulrich Kilian <uk@sdcience-and-more.de>
2026-07-23 15:46:53 +0200

    feat(sale_expense_unlink): migrate

---
+++ sale_expense_unlink/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Sale Expense Unlink",
+    "summary": """
+        Allow deletion of sale order expense lines.
+    """,
+    "author": "Mint System GmbH",
+    "website": "https://github.com/Mint-system/",
+    "category": "Sales",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["sale_expense_link"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+    "qweb": ["static/src/xml/board.xml"],
+}
