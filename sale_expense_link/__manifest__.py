91a18e799e2b30cc7c33ccae024045f42e933710
Ulrich Kilian <uk@sdcience-and-more.de>
2026-07-23 15:36:08 +0200

    feat(sale_expense_link): migrate

---
+++ sale_expense_link/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Sale Expense Link",
+    "summary": """
+        Link expense and sale order line.
+    """,
+    "author": "Mint System GmbH",
+    "website": "https://github.com/Mint-system/",
+    "category": "Sales",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["sale_expense"],
+    "data": ["views/hr_expense_sheet.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+    "qweb": ["static/src/xml/board.xml"],
+}
