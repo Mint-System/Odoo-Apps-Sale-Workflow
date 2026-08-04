3cd9dff8c4212f1c65cb5c40b8131d7c0ae3c4b9
Ulrich Kilian <uk@sdcience-and-more.de>
2026-08-04 15:37:11 +0200

    feat(sale_subscription_disable_unpaid_closing): filter unpaid from expired subscriptions

--- sale_subscription_disable_unpaid_closing/models/sale_order.py
+++ sale_subscription_disable_unpaid_closing/models/sale_order.py
@@ -12,3 +12,9 @@ class SaleOrder(models.Model):
 
     def _handle_unpaid_subscriptions(self):
         return {}
+
+
+    def _get_expired_subscriptions(self):
+        expired_result = super()._get_expired_subscriptions()
+        unpaid_ids = {r['so_id'] for r in self._get_unpaid_subscriptions()}
+        return [r for r in expired_result if r['so_id'] not in unpaid_ids]
