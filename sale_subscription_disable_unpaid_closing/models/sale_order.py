a50989568e53d295a4fe019d241fec679286a48e
Ulrich Kilian <uk@sdcience-and-more.de>
2026-08-04 17:47:17 +0200

    feat(sale_subscription_disable_unpaid_closing): make unclosing configurable

--- sale_subscription_disable_unpaid_closing/models/sale_order.py
+++ sale_subscription_disable_unpaid_closing/models/sale_order.py
@@ -10,11 +10,27 @@ _logger = logging.getLogger(__name__)
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    def _handle_unpaid_subscriptions(self):
-        return {}
+    def _skip_unpaid_close(self):
+        return self.env['ir.config_parameter'].sudo().get_param(
+            'sale_subscription_disable_unpaid_closing.skip_unpaid_subscription_close', 'True'
+        ) == 'True'
 
+    def _skip_expired_close(self):
+        return self.env['ir.config_parameter'].sudo().get_param(
+            'sale_subscription_disable_unpaid_closing.skip_expired_subscription_close', 'True'
+        ) == 'True'
+
+
+    def _handle_unpaid_subscriptions(self):
+        if self._skip_unpaid_close():
+            return {}
+        return super()._handle_unpaid_subscriptions()
 
     def _get_expired_subscriptions(self):
+        if self._skip_expired_close():
+            return []
         expired_result = super()._get_expired_subscriptions()
-        unpaid_ids = {r['so_id'] for r in self._get_unpaid_subscriptions()}
-        return [r for r in expired_result if r['so_id'] not in unpaid_ids]
+        if self._skip_unpaid_close():
+            unpaid_ids = {r['so_id'] for r in self._get_unpaid_subscriptions()}
+            expired_result = [r for r in expired_result if r['so_id'] not in unpaid_ids]
+        return expired_result
