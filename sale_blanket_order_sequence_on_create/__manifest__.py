edbd024927379269eaffac006ac6c04b326e2ce8
Ulrich Kilian <uk@sdcience-and-more.de>
2026-09-18 17:22:50 +0200

    feat(sale_blanket_order_sequence_on_create): init

---
+++ sale_blanket_order_sequence_on_create/__manifest__.py
@@ -0,0 +1,20 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Blanket Order Sequence On Create",
+    "summary": """
+        Assign sequence if blanket order is created.
+    """,
+    "author": "Mint System GmbH",
+    "website": "https://www.mint-system.ch/",
+    "category": "Repository",
+    "development_status": "Production/Stable",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["sale_blanket_order"],
+    "data": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+    
+}
---
+++ sale_blanket_order_sequence_on_create/models/__init__.py
@@ -0,0 +1,2 @@
+
+from . import sale_blanket_order
